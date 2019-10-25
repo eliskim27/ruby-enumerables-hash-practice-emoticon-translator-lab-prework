e9d08812f33e06d8e983b52ccc0afd5a4a652869
Eli Kim <eliskim27@gmail.com>
2019-10-25 02:43:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file, emoticon)
   load_library(file)
   jap_emotes = {}
   
-  jap_emotes << new_hash["get_emoticon"][emoticon]
+  jap_emotes = new_hash["get_emoticon"][emoticon]
   
   
   jap_emotes
