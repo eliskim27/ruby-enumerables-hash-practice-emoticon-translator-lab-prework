afcd8b066c62688433841a7627da6f2293178bcc
Eli Kim <eliskim27@gmail.com>
2019-10-25 02:52:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 def get_japanese_emoticon(file, emoticon)  
   jap_emotes = load_library(file)["get_emoticon"][emoticon]
                       #return value for laod_library is new_hash
-  load_library(file)["get_emoticon"].contains? (emoticon)
+  load_library(file)["get_emoticon"].key? (emoticon)
   
   
 end
